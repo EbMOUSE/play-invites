7f9421165b84fd1fbfe5d84e3a8d050337c8f3fa
MegijaB <Megja@inbox.lv>
2024-10-30 22:49:30 +0200

    mainīts web

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,20 @@
 <?php
 
-use App\Http\Controllers\FirstController;
-use App\Http\Controllers\MainController;
-use App\Http\Controllers\InviteController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/default', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get( '/', [MainController::class , 'index'] );
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::post( '/sign_up', [UserController::class , 'sign_up'] )->name('user.sign_up');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::post( '/sign_in', [UserController::class , 'sign_in'] )->name('user.sign_in');
-
-Route::post( '/invite', [InviteController::class , 'store'] )->name('invite.post');
-
-Route::get( '/profile', [ProfileController::class ,'index'] );
-
-/*
-Example: 127.0.0.1:8000/profile
-Route::get( '/profile', [ProfileController::class , 'index'] );
-
-^ CON-TROLL-ERS. Good for organisation! :p
-Try to use them instead of cramming code inside this file
-*/
+require __DIR__.'/auth.php';
